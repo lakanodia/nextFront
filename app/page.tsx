fd92d29f328ac8906734d52c04e56170ecf63dcc
Laka Nodia <laka.nodia@gmail.com>
2025-10-07 21:43:37 +0400

    Change a tah to Link

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import ServicesSection from "./components/ServicesSection";
 import SubscribeSection from "./components/SubscribeSection";
 import ManagementSection from "./components/ManagementSection";
 import { Publication } from "./types/publication";
+import Link from "next/link";
 
 export default async function Home() {
   const res = await fetch(`${apiUrl}/api/blogs?populate=cover&locale=ka`);
@@ -23,12 +24,12 @@ export default async function Home() {
       <ServicesSection />
       <div className="flex items-center justify-between mb-8">
         <h1 className="text-4xl font-bold text-[#23313b]">Reports</h1>
-        <a
+        <Link
           href="/publications"
           className="text-green-600 font-semibold text-lg flex items-center gap-2 hover:underline"
         >
           View more <span className="text-xl">&#8594;</span>
-        </a>
+        </Link>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {publications.map((d: Publication) => (
