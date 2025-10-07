b24e786b66c2f96d2512d6f634359bc022b97121
demonno <demur.nodia@gmail.com>
2025-10-07 21:35:58 +0400

    Add config repo

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   trailingSlash: true,
+  basePath: isGithubActions ? `/${repo}` : "",
+  assetPrefix: isGithubActions ? `/${repo}` : "",
   images: {
     unoptimized: true,
     remotePatterns: [
