7d98348b3ae009b56daa4ae70f456ed7831b93e5
demonno <demur.nodia@gmail.com>
2025-10-07 21:29:48 +0400

    Adjust config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
-import { apiUrl } from "./constants";
-
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  allowedDevOrigins: [`${apiUrl}`],
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
